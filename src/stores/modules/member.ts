7061a5cd2c8460479918a2d1b635e8b578c9fc99
zgcwf <1935083614@qq.com>
2023-12-11 19:11:50 +0800

    登录

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -1,45 +1,44 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import type { LoginResult } from "@/types/member";
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
 // 定义 Store
 export const useMemberStore = defineStore(
-  'member',
+  "member",
   () => {
     // 会员信息
-    const profile = ref<any>()
+    const profile = ref<LoginResult>();
 
     // 保存会员信息，登录时使用
-    const setProfile = (val: any) => {
-      profile.value = val
-    }
+    const setProfile = (val: LoginResult) => {
+      profile.value = val;
+    };
 
     // 清理会员信息，退出时使用
     const clearProfile = () => {
-      profile.value = undefined
-    }
+      profile.value = undefined;
+    };
 
     // 记得 return
     return {
       profile,
       setProfile,
       clearProfile,
-    }
+    };
   },
-  // TODO: 持久化
   {
     // 网页端配置
     // persist: true,
-
-    // uni配置, 调整为兼容多端的API
+    // 小程序端配置
     persist: {
       storage: {
-        setItem(key, value) {
-          uni.setStorageSync(key, value)
-        },
         getItem(key) {
-          return uni.getStorageSync(key)
+          return uni.getStorageSync(key);
+        },
+        setItem(key, value) {
+          uni.setStorageSync(key, value);
         },
       },
     },
   },
-)
+);
--- src/types/member.d.ts
+++ src/types/member.d.ts
@@ -1,46 +1,43 @@
 /** 通用的用户信息 */
 type BaseProfile = {
   /** 用户ID */
-  id: number
+  id: number;
   /** 头像  */
-  avatar: string
+  avatar: string;
   /** 账户名  */
-  account: string
+  account: string;
   /** 昵称 */
-  nickname?: string
-}
+  nickname?: string;
+};
 
 /** 小程序登录 登录用户信息 */
 export type LoginResult = BaseProfile & {
   /** 手机号 */
-  mobile: string
+  mobile: string;
   /** 登录凭证 */
-  token: string
-}
+  token: string;
+};
 
 /** 个人信息 用户详情信息 */
 export type ProfileDetail = BaseProfile & {
   /** 性别 */
-  gender?: Gender
+  gender?: Gender;
   /** 生日 */
-  birthday?: string
+  birthday?: string;
   /** 省市区 */
-  fullLocation?: string
+  fullLocation?: string;
   /** 职业 */
-  profession?: string
-}
+  profession?: string;
+};
 /** 性别 */
-export type Gender = '女' | '男'
+export type Gender = "女" | "男";
 
 /** 个人信息 修改请求体参数 */
-export type ProfileParams = Pick<
-  ProfileDetail,
-  'nickname' | 'gender' | 'birthday' | 'profession'
-> & {
+export type ProfileParams = Pick<ProfileDetail, "nickname" | "gender" | "birthday" | "profession"> & {
   /** 省份编码 */
-  provinceCode?: string
+  provinceCode?: string;
   /** 城市编码 */
-  cityCode?: string
+  cityCode?: string;
   /** 区/县编码 */
-  countyCode?: string
-}
+  countyCode?: string;
+};
