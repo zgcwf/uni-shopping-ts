8fc2e749cdcdd2c910cd17596b1056c4c349732b
zgcwf <1935083614@qq.com>
2024-01-02 16:33:47 +0800

    字体图标与购物车模块

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -228,8 +228,12 @@ const onBuyNow = (ev: SkuPopupEvent) => {
   <view class="toolbar" :style="{ bottom: safeAreaInsets?.bottom + 'px' }">
     <view class="icons">
       <button class="icons-button"><text class="icon-heart"></text>收藏</button>
+      <!-- #ifdef MP-WEIXIN -->
       <button class="icons-button" open-type="contact"><text class="icon-handset"></text>客服</button>
-      <navigator class="icons-button"><text class="icon-cart"></text>购物车</navigator>
+      <!-- #endif -->
+      <navigator class="icons-button" url="/pages/cart/cart2" open-type="navigate">
+        <text class="icon-cart"></text>购物车
+      </navigator>
     </view>
     <view class="buttons">
       <view class="addcart" @tap="openSkuPopup(SkuMode.Cart)"> 加入购物车 </view>
